d6ff7a8366dc01193c09e3b46ae8a54e085dff48
ammo <solidifyarmor@gmail.com>
2023-05-29 01:29:20 +0630

    setup mysql

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -15,11 +18,19 @@ type application struct {
 func main() {
 	// config
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	// db
+	dsn := flag.String("dsn", "web:passpass@/snippetbox?parseTime=true", "MySQL database connection")
 	flag.Parse()
 
 	// logger
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	// app instance with di
 	app := &application{
@@ -35,6 +46,17 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s\n", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
