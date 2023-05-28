c36da8df899b47594121912446ab017255e5b108
ammo <solidifyarmor@gmail.com>
2023-05-28 19:16:55 +0630

    loggers

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,21 +4,36 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	// config
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
+	// logger
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// handlers
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	// static file server
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Printf("Starting server on %s\n", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	// server
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting server on %s\n", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
