ea62c69ed4516083f5354af1e518bb269cf135c4
ammo <solidifyarmor@gmail.com>
2023-05-31 21:48:15 +0630

    middle chain package

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,8 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 func (app *application) routes() http.Handler {
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", app.home)
@@ -12,5 +17,6 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	// return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	return standardMiddleware.Then(mux)
 }
